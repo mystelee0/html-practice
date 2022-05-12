6864383064026117d30a67c574b59f9bde0a3a06
mystelee0 <99077666+mystelee0@users.noreply.github.com>
2022-05-13 00:17:39 +0900

    게시글 불러오기 미완성

---
+++ index_show_list.php
@@ -0,0 +1,27 @@
+<?php
+$con=mysqli_connect("localhost","root","","board");
+if(!$con)
+{
+    die("리스트를 불러오기 위한 데이터베이스 연결 실패".mysqli_connect_error());
+}
+//$sql="select * from boards order by num desc limit 5";
+$sql="select * from boards";
+$result=mysqli_query($con,$sql);
+
+$record_number=mysqli_num_rows($result);
+if($record_number){
+//$i=$record_number/5;
+//$j=$record_number%5;
+        $row=mysqli_fetch_array($result);
+        $response="";
+        $response="
+        <tr>
+        <td>".$row["title"]."</td>
+        <td>".$row["nname"]."</td>
+        <td>".$row["regist_day"]."</td>
+        </tr>
+        ";
+        
+    echo json_encode($response);
+}
+?>
